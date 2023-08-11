de4a216228a25413c42a6012be86847fe426c734
Daniel Merritt <daniel.mafou@gmail.com>
2023-08-11 10:54:17 -0400

    index route and controller

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -18,7 +18,19 @@ async function create(req,res) {
   }
 }
 
+async function index(req, res) {
+  try {
+    const blogs = await Blog.find({})
+      .populate('author')
+      .sort({ createdAt: 'desc' })
+    res.status(200).json(blogs)
+  } catch (error) {
+    res.status(500).json(error)
+  }
+}
+
 
 export { 
-  create
+  create,
+  index
 }
